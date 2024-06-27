0f82ecb1b5fe342ec1df694afc1a8875a2551748
Ludovico Passari <ludovicopassari9@gmail.com>
2024-06-27 21:38:01 +0200

    ciao

--- src/main/java/com/restapi/IndoorAtmosphereControl/entity/UserInfo.java
+++ src/main/java/com/restapi/IndoorAtmosphereControl/entity/UserInfo.java
@@ -6,7 +6,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
-/**/
+
 
 @Entity
 @Table(name="users")
