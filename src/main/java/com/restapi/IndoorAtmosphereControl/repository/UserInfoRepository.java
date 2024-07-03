6c29ee399357afe546df24fd761718ecfcd2ba93
Ludovico Passari <ludovicopassari9@gmail.com>
2024-07-03 22:37:27 +0200

    dockerfile update

--- src/main/java/com/restapi/IndoorAtmosphereControl/repository/UserInfoRepository.java
+++ src/main/java/com/restapi/IndoorAtmosphereControl/repository/UserInfoRepository.java
@@ -2,9 +2,11 @@ package com.restapi.IndoorAtmosphereControl.repository;
 
 import com.restapi.IndoorAtmosphereControl.entity.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserInfoRepository extends JpaRepository<UserInfo,Long> {
 
     Optional<UserInfo> findByUsername(String username);
